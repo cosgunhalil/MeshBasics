86c9a9d21af7c75be3b307cca149e0da085bf3bf
Halil Coşgun <cosgun.halil@gmail.com>
2020-03-08 15:18:19 +0300

    feat: generate procedural grid
    Procedural Grid tutorial by Jasper Flick is completed.
    
    https://catlikecoding.com/unity/tutorials/procedural-grid/

--- MeshBasics/Assets/MeshBasics/ProceduralGrid/Scripts/Grid.cs
+++ MeshBasics/Assets/MeshBasics/ProceduralGrid/Scripts/Grid.cs
@@ -26,14 +26,23 @@ namespace MeshBasics.ProceduralGrid
             mesh.name = "Procedural Grid";
 
             vertices = new Vector3[(xSize + 1) * (ySize + 1)];
+            Vector2[] uv = new Vector2[vertices.Length];
+            Vector4[] tangents = new Vector4[vertices.Length];
+            Vector4 tangent = new Vector4(1f, 0f, 0f, -1f);
+
             for (int i = 0, y = 0; y <= ySize; y++)
             {
                 for (int x = 0; x <= xSize; x++, i++)
                 {
                     vertices[i] = new Vector3(x, y);
+                    uv[i] = new Vector2((float)x / xSize, (float)y / ySize);
+                    tangents[i] = tangent;
                 }
             }
+
             mesh.vertices = vertices;
+            mesh.uv = uv;
+            mesh.tangents = tangents;
 
             int[] triangles = new int[xSize * ySize * 6];
             for (int ti = 0, vi = 0, y = 0; y < ySize; y++, vi++)
@@ -46,7 +55,9 @@ namespace MeshBasics.ProceduralGrid
                     triangles[ti + 5] = vi + xSize + 2;
                 }
             }
+
             mesh.triangles = triangles;
+            mesh.RecalculateNormals();
         }
     }
 }
